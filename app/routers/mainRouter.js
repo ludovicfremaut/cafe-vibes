dfbe82faa392efe3600dcb00ce80a70457a716fc
Ludovic Fremaut <ludovic.fremaut@hotmail.fr>
2025-07-19 18:03:24 +0200

    feat: create page catalog && contact

--- app/routers/mainRouter.js
+++ app/routers/mainRouter.js
@@ -7,4 +7,16 @@ mainRouter.get('/', (req, res) => {
   res.render('home');
 });
 
+mainRouter.get('/about', (req, res) => {
+  res.render('about');
+});
+
+mainRouter.get('/catalog', (req, res) => {
+  res.render('catalog');
+});
+
+mainRouter.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
 export default mainRouter;
